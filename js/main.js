c6f600a94844d1a0a08a0c19844802a6b17e2805
Matt Carapazza <matthew.carapazza@gmail.com>
2021-04-23 14:37:43 -0400

    changed styles.scss to be strictly for import, changed hero around for glass look, settled on color pallet

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 const emailSwitch = document.getElementById('email-switch');
 const landingPagesSwitch = document.getElementById('pages-switch');
+const websitesSwitch = document.getElementById('websites-switch');
 const emailContainer = document.getElementById('emails');
 const landingPageContainer = document.getElementById('landing-pages');
+const websitesContainer = document.getElementById('websites');
 
 emailSwitch.addEventListener('click', () => {
     emailContainer.style.display = 'grid';
     landingPageContainer.style.display = 'none';
-})
+    websitesContainer.style.display = 'none';
+});
 
 landingPagesSwitch.addEventListener('click', () => {
     emailContainer.style.display = 'none';
     landingPageContainer.style.display = 'grid';
-})
+    websitesContainer.style.display = 'none';
+});
+
+websitesSwitch.addEventListener('click', () => {
+    emailContainer.style.display = 'none';
+    landingPageContainer.style.display = 'none';
+    websitesContainer.style.display = 'grid';
+});
